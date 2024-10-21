7512c3b7b72cb7d97324231f7ca6be508bfb0a2c
hienhg <giahienhoang99@gmail.com>
2024-10-21 17:12:46 -0400

    getUsersForSidebar in user controller can get all users for displaying in the sidebar

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        // fetch all users from db (without password in params)
+        const allUsers = await User.find().select("-password");
+
+        res.status(200).json(allUsers);
+
+    } catch (error) {
+        console.log("Error in getUsersForSidebar in user controller:", error.message);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+}
