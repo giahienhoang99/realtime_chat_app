3ae199db30f33db92091d9ec6f1064678b571f08
hienhg <giahienhoang99@gmail.com>
2024-10-22 23:52:19 -0400

    structuring frontend folder + new pages

---
+++ frontend/src/pages/login/LogIn.jsx
@@ -0,0 +1,14 @@
+const LogIn = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg shdow-md bg-gray-400 bg-clip-padding'>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                    Login
+                    <span className='text-blue-500'> ChatApp</span>
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default LogIn;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	
+	plugins: [require("daisyui")],
+};
 
