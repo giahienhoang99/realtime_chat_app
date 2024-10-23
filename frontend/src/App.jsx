5a36e872f3c3f0f6d058f45989e981ae94986993
hienhg <giahienhoang99@gmail.com>
2024-10-23 01:19:13 -0400

    ui: log in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import LogIn from './pages/login/Login';
 
 function App() {
 
-  return <div className='p-4 h-screen flex items-center justify-center'>
-    <LogIn/>
-  </div>
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <LogIn />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/login/LogIn.jsx
+++ frontend/src/pages/login/LogIn.jsx
@@ -1,11 +1,40 @@
 const LogIn = () => {
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-            <div className='w-full p-6 rounded-lg shdow-md bg-gray-400 bg-clip-padding'>
-                <h1 className='text-3xl font-semibold text-center text-gray-300'>
-                    Login
-                    <span className='text-blue-500'> ChatApp</span>
+            <div className='w-full p-6 rounded-lg shdow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+                <h1 className='text-3xl font-semibold text-center text-white'>
+                    Log in to your
+                    <span className='text-rose-400'> ChatApp</span>
                 </h1>
+
+                <form>
+                    <div>
+                        <label className="label p-2">
+                            <span className="text-base label-text text-white">Username</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter your username"
+                            className="w-full input input-bordered text-rose-400 h-10 placeholder:text-rose-400"
+                        />
+                    </div>
+                    <div>
+                        <label className="label p-2">
+                            <span className="text-base label-text text-white">Password</span>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Enter your password"
+                            className="w-full input input-bordered text-rose-400 h-10 placeholder:text-rose-400"
+                        />
+                    </div>
+                    <a href="#" className="text-xs text-white hover:underline hover:text-rose-300 mt-2 inline-block">
+                        {"Don't"} have an account yet?
+                    </a>
+                    <div>
+                        <button className="btn btn-block btn-sm mt-2 text-rose-400">Log in</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
