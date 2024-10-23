7a408de0d1ba9d20450c265a085843d2662411eb
hienhg <giahienhoang99@gmail.com>
2024-10-23 02:18:33 -0400

    ui: use radio buttons instead of check box so users can choose only one gender

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -2,15 +2,25 @@ const GenderCheckbox = () => {
     return (
         <div className="flex">
             <div className="form-control">
-                <label className={"label gap-2 cursor-pointer"}>
-                    <span className="label-text text-white">Male</span>
-                    <input type="checkbox" className="checkbox checkbox-sm border-rose-400 [--chkbg:theme(colors.rose.200)] [--chkfg:theme(colors.rose.400)] checked:border-rose-400" />
+                <label className="label gap-2 cursor-pointer">
+                    <span className="label-text text-white text-sm">Male</span>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        className="radio radio-xs border-rose-400 checked:bg-rose-400 checked:border-rose-400"
+                    />
                 </label>
             </div>
             <div className="form-control">
-                <label className={"label gap-2 cursor-pointer"}>
-                    <span className="label-text text-white">Female</span>
-                    <input type="checkbox" className="checkbox checkbox-sm border-rose-400 [--chkbg:theme(colors.rose.200)] [--chkfg:theme(colors.rose.400)] checked:border-rose-400" />
+                <label className="label gap-2 cursor-pointer">
+                    <span className="label-text text-white text-sm">Female</span>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        className="radio radio-xs border-rose-400 checked:bg-rose-400 checked:border-rose-400"
+                    />
                 </label>
             </div>
         </div>
