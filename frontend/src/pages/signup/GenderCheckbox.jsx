903e8c5422518a7fc79ef32b70c0bc1d05e19fc4
hienhg <giahienhoang99@gmail.com>
2024-10-23 02:26:00 -0400

    make comment for starter code

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -28,3 +28,38 @@ const GenderCheckbox = () => {
 }
 
 export default GenderCheckbox;
+
+/*
+STARTER CODE FOR GENDER CHECKBOX
+
+const GenderCheckbox = () => {
+    return (
+        <div className="flex">
+            <div className="form-control">
+                <label className="label gap-2 cursor-pointer">
+                    <span className="label-text text-white text-sm">Male</span>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        className="radio radio-xs border-rose-400 checked:bg-rose-400 checked:border-rose-400"
+                    />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className="label gap-2 cursor-pointer">
+                    <span className="label-text text-white text-sm">Female</span>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        className="radio radio-xs border-rose-400 checked:bg-rose-400 checked:border-rose-400"
+                    />
+                </label>
+            </div>
+        </div>
+    );
+}
+
+export default GenderCheckbox;
+*/
