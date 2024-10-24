6ad442646de03558134444a967757c614814e6f4
hienhg <giahienhoang99@gmail.com>
2024-10-24 17:24:32 -0400

    ui-sidebar: search icon added next to search bar

--- frontend/src/components/sidebar/SearchBar.jsx
+++ frontend/src/components/sidebar/SearchBar.jsx
@@ -1,8 +1,11 @@
+import { IoSearch } from "react-icons/io5";
+
 const SearchBar = () => {
   return (
     <form className="flex items-center gap-2">
       <input type="text" placeholder="Search here ..." 
       className="input input-bordered input-primary w-full max-w-xs placeholder:text-violet-300 text-violet-400" />
+      <button type="submit" className="btn btn-circle bg-black bg-opacity-85 text-violet-400"><IoSearch /></button>
     </form>
   )
 }
