743713b04ba8004cf0b7ff32314099a8417d0058
hienhg <giahienhoang99@gmail.com>
2024-10-25 15:20:37 -0400

    ui-home:sidebar resized

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-white bg-clip-padding backdrop-filter backdrop-blur-0'>
+    <div className='flex h-[90%] rounded-lg overflow-hidden bg-white bg-clip-padding backdrop-filter backdrop-blur-0'>
       <Sidebar />
     </div>
   );
