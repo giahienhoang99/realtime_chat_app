c5871209d272a36a1e73fc8c308ddc76c6905f00
hienhg <giahienhoang99@gmail.com>
2024-10-25 16:36:09 -0400

    ui-home:messagecontainer header

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,8 +2,8 @@
 // message container aka chatbox
 const MessageContainer = () => {
     return (
-        <div className="bg-white w-[1000px] px-4 py-2 mb-2">
-            <div className="flex gap-2 items-center px-2 py-2">
+        <div className="bg-white w-[1000px] px-2 py-2 mb-2">
+            <div className="flex gap-2 items-center px-2">
                 <div className="avatar online">
                     <div className="w-12 bg-rose-300 rounded-full">
                         <img src="https://avatar.iran.liara.run/public/49" alt="user avatar" />
@@ -11,7 +11,7 @@ const MessageContainer = () => {
                 </div>
                 <span className="text-black font-bold text-opacity-70">Hien Beo</span>
             </div>
-            <div className="divider mt-0"/>
+            <div className="divider my-0 mx-1.5"/>
         </div>
     );
 };
