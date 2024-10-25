2738d1d093ed6784b66bd991cde431b87f2495b4
hienhg <giahienhoang99@gmail.com>
2024-10-24 23:26:24 -0400

    ui-searchbar: light mode

--- frontend/src/components/sidebar/SearchBar.jsx
+++ frontend/src/components/sidebar/SearchBar.jsx
@@ -3,9 +3,12 @@ import { IoSearch } from "react-icons/io5";
 const SearchBar = () => {
   return (
     <form className="flex items-center gap-2">
-      <input type="text" placeholder="Search here ..."
-        className="input input-bordered input-primary w-full max-w-xs placeholder:text-violet-300 text-violet-400" />
-      <button type="submit" className="btn btn-circle bg-white hover:bg-rose-400 hover:text-white bg-opacity-85 text-rose-400">
+      <input
+        type="text"
+        placeholder="Search here ..."
+        className="input input-bordered text-rose-400 bg-white border-rose-400 input-primary w-full max-w-xs placeholder:text-rose-400 text-rose-400 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-rose-300"
+      />
+      <button type="submit" className="btn btn-circle border-rose-400 bg-white hover:bg-rose-400 hover:border-rose-400 hover:text-white bg-opacity-85 text-rose-400">
         <IoSearch className="w-6 h-6 outline-none" />
       </button>
       {/* Button for dark mode */}
