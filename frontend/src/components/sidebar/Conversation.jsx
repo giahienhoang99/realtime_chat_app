fb0ad592eee267498df333ac7bbbd181476938cc
hienhg <giahienhoang99@gmail.com>
2024-10-24 23:43:18 -0400

    ui-sidebar: conversations aka user's chats

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,18 +1,16 @@
 const Conversation = () => {
     return (
         <>
-            <div className="flex gap-2 items-center hover:bg-rose-300 rounded p-2 py-1 curs">
+            <div className="flex gap-2 items-center hover:bg-rose-400 hover:text-white text-black text-opacity-70 rounded p-2 py-2 curs">
                 <div className="avatar online">
-                    <div className="w-24 rounded-full">
-                        <img src="https://img.daisyui.com/images/stock/
-                        photo-1534528741775-53994a69daeb.webp" alt="user avatar"
-                        />
+                    <div className="w-12 rounded-full">
+                        <img src="https://avatar.iran.liara.run/public/38" alt="user avatar"/>
                     </div>
                 </div>
 
                 <div className="flex flex-col flex-1">
                     <div className="flex gap-3 justify-between">
-                        <p className="font-bold text-grey">John Doe</p>
+                        <p className="font-bold">Hien Beo</p>
                     </div>
                 </div>
             </div>
