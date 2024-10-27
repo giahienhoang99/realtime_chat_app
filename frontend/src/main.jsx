3c8dfb3c250561d3bc0f349a6906cfac4935c12b
hienhg <giahienhoang99@gmail.com>
2024-10-27 14:23:58 -0400

    add explanations in comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+/*
+BrowserRouter from react-router-dom, adding SPA routing capabilities
+
+The app can now use client-side navigation, which improves user experience by avoiding full page reloads 
+and allows you to manage different views in your real-time chat app, like logging in, chatting, and 
+viewing other users
+
+BrowserRouter uses the HTML5 history API to keep your app’s URL in sync with the content displayed on 
+the screen. When users click a link or trigger a navigation action, React Router updates the URL and 
+displays the appropriate component without reloading the page
+*/
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
