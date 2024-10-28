122df1310170e490a5c175bf612529f5676cd89c
hienhg <giahienhoang99@gmail.com>
2024-10-27 23:41:11 -0400

    signup page: useState logic for signup page

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
 
 const SignUp = () => {
+
+    const [inputs, setInputs] = useState({
+        fullName: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+        gender: ''
+    });
+
     return (
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -17,6 +27,14 @@ const SignUp = () => {
                             type="text"
                             placeholder="Joe Mama"
                             className="w-full text-rose-400 input input-bordered h-8 placeholder:text-rose-400"
+                            value={inputs.fullName}
+                            // e = change event
+                            // e.target = refers to the element that triggered the event (in this case, the input field)
+                            // e.target.value = value of the input field
+                            // ... is the spread operator in js => ...inputs means to copy all fields in inputs into a new obj
+                            onChange={(e) => setInputs({...inputs, fullName: e.target.value})}
+                            // this line means => on the event that input field is changed, set or update the input to a new obj
+                            // with fields copied from the predefined inputs but the fullName is overrided with the newly entered val in the field
                         />
                     </div>
                     <div>
@@ -27,6 +45,8 @@ const SignUp = () => {
                             type="text"
                             placeholder="joemama"
                             className="w-full bg-black bg-opacity-60 text-rose-400 input input-bordered h-8 placeholder:text-rose-400"
+                            value={inputs.username}
+                            onChange={(e) => setInputs({...inputs, username: e.target.value})}
                         />
                     </div>
                     <div>
@@ -37,6 +57,8 @@ const SignUp = () => {
                             type="password"
                             placeholder="Enter your password"
                             className="w-full bg-black bg-opacity-60 text-rose-400 input input-bordered h-8 placeholder:text-rose-400"
+                            value={inputs.password}
+                            onChange={(e) => setInputs({...inputs, password: e.target.value})}
                         />
                     </div>
                     <div>
@@ -47,6 +69,8 @@ const SignUp = () => {
                             type="password"
                             placeholder="Re-enter your password"
                             className="w-full bg-black bg-opacity-60 text-rose-400 input input-bordered h-8 placeholder:text-rose-400"
+                            value={inputs.confirmPassword}
+                            onChange={(e) => setInputs({...inputs, confirmPassword: e.target.value})}
                         />
                     </div>
 
