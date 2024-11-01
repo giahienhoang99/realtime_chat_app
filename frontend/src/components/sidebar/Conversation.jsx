fd293060eae236b5a2d75d20b90ccf8a6dad0072
hienhg <giahienhoang99@gmail.com>
2024-10-31 22:22:39 -0400

    can get conversations for user now

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,20 +1,20 @@
-const Conversation = () => {
+const Conversation = ({conversation,lastIdx}) => {
     return (
         <>
             <div className="flex gap-2 items-center hover:bg-rose-400 hover:text-white text-black text-opacity-70 rounded p-2 py-2 curs">
                 <div className="avatar online">
                     <div className="w-12 bg-rose-300 rounded-full">
-                        <img src="https://avatar.iran.liara.run/public/49" alt="user avatar" />
+                        <img src={conversation.profilePic} alt="user avatar" />
                     </div>
                 </div>
 
                 <div className="flex flex-col flex-1">
                     <div className="flex gap-3 justify-between">
-                        <p className="font-bold">Hien Beo</p>
+                        <p className="font-bold">{conversation.fullName}</p>
                     </div>
                 </div>
             </div>
-            <div className="divider my-0 py-0 h-1"></div>
+            {lastIdx ? null : <div className="divider my-0 py-0 h-1"></div>}
         </>
     );
 };
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -3,14 +3,18 @@ import Conversation from "./Conversation";
 
 const Conversations = () => {
   const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATIONS:", conversations);
 
   return (
     <div className="mt-0 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation.id}
+          conversation={conversation}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   );
 };
