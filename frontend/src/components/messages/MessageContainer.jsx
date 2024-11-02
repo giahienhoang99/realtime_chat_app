8c75a917bf0b7ea4ea661318835b6b99cec4c964
hienhg <giahienhoang99@gmail.com>
2024-11-02 00:11:38 -0400

    minor changes

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -45,7 +45,7 @@ const NoChatSelected = () => {
         <div className='flex items-center justify-center w-full h-full'>
             <div className='px-4 text-center sm:text-lg md:text-xl text-rose-400 font-semibold flex flex-col
              items-center gap-2'>
-                <p>Welcom to ChatApp, Hien Beo XL</p>
+                <p>Welcome to RealTimeChatApp</p>
                 <p>Select a chat to start messaging</p>
                 <TiMessages className="text-3xl md:text-6xl text-center" />
             </div>
