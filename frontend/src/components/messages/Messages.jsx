51058fa98f3b78a04b7bac02467c54fa6c872b26
hienhg <giahienhoang99@gmail.com>
2024-11-04 01:09:17 -0500

    ui: messages and message components ui and logic fix

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,24 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
+
 const Messages = () => {
   const { messages, loading } = useGetMessages();
   console.log("messages:", messages);
 
   return (
     <div className="px-2 flex-1 overflow-auto">
+      {!loading && messages.length > 0 && messages.map((message) => (
+        <Message key={message._id} message={message} />
+      ))}
 
       {/* skeleton when loading message data from db */}
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
       {/* if conversation has no message */}
       {!loading && messages.length === 0 && (
-        <p className="text-center">Send a message to start the conversation</p>
+        <p className="text-center text-rose-400">Send a message to start the conversation</p>
       )}
-
     </div>
   );
 };
