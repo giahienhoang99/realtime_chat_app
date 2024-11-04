816485a04759dfd66baadd523329b28e283e8aa5
hienhg <giahienhoang99@gmail.com>
2024-11-03 23:57:51 -0500

    message skeleton while messages are being fetched from db

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,12 +1,20 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
-
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 const Messages = () => {
   const { messages, loading } = useGetMessages();
   console.log("messages:", messages);
 
   return (
     <div className="px-2 flex-1 overflow-auto">
+
+      // skeleton when loading message data from db
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      // if conversation has no message
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
       
     </div>
   );
