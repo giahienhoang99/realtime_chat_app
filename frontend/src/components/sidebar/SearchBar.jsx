7c3d55a47708a74cd62d5c127d9eccee6c5889ce
hienhg <giahienhoang99@gmail.com>
2024-11-06 00:05:10 -0500

    searchbar: search feature with at least 3 characters in input

--- frontend/src/components/sidebar/SearchBar.jsx
+++ frontend/src/components/sidebar/SearchBar.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchBar = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error('Search term must be longer than 3 characters');
+    }
+
+    const conversation = conversation.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      return toast.error("No such user found");
+    }
+  }
+
   return (
-    <form className="h-8 flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="h-8 flex items-center gap-2">
       <input
         type="text"
         placeholder="Search here ..."
         className="input input-bordered text-rose-400 bg-white border-rose-400 input-primary w-full max-w-xs 
         placeholder:text-rose-300 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:border-rose-400"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       {/* Searchbar for dark mode */}
       {/* <input
