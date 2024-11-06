86ebead1a03b0b039bb07b793e2483bb99954409
hienhg <giahienhoang99@gmail.com>
2024-11-05 23:08:05 -0500

    chat is always scrolled down to the bottom when opened

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,15 +1,26 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
+import { useEffect, useRef } from "react";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  console.log("messages:", messages);
+  //console.log("messages:", messages);
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    // Scroll to the last message whenever messages change
+    if (messages.length > 0) {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  },[messages]);
 
   return (
     <div className="px-2 flex-1 overflow-auto">
       {!loading && messages.length > 0 && messages.map((message) => (
-        <Message key={message._id} message={message} />
+        <div key={message._id} ref={lastMessageRef}>
+          <Message message={message} />
+        </div>
       ))}
 
       {/* skeleton when loading message data from db */}
