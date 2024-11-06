5f57147cb3a9f67909f7d9f02949a8eb0a9f08a3
hienhg <giahienhoang99@gmail.com>
2024-11-05 20:50:18 -0500

    function to extract time for messageCreatedAt

---
+++ frontend/src/utils/extractTime.js
@@ -0,0 +1,10 @@
+export function extractTime(dateString) {
+    const date = new Date(dateString);
+    const hours = padZero(date.getHours());
+    const mins = padZero(date.getMinutes());
+    return `${hours}:${mins}`;
+}
+// pad single-digit nuymbers with a leading zero
+function padZero(number) {
+    return number.toString().padStart(2,"0");
+}
