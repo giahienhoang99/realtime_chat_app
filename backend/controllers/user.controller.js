d09b1e45489b28e03d2dce609134abd637846977
hienhg <giahienhoang99@gmail.com>
2024-11-05 21:01:40 -0500

    remove the current logged in user from the conversations on sidebar

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
 
-export const getUsersForSidebar = async (req,res) => {
+export const getUsersForSidebar = async (req, res) => {
     try {
-        //const loggedInUserId = req.user._id;
-        // fetch all users from db (without password in params)
-        const allUsers = await User.find().select("-password");
+        const loggedInUserId = req.user._id;
+        // fetch all users from db except the current logged in user (without password in params)
+        const allUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
         res.status(200).json(allUsers);
 
     } catch (error) {
         console.log("Error in getUsersForSidebar in user controller:", error.message);
-        res.status(500).json({ error: "Internal Server Error"});
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
