d2f43b8522dbd95e8c070a92975d5acd5008ff6d
hienhg <giahienhoang99@gmail.com>
2024-11-07 21:57:16 -0500

    backend: update socket.io server in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { server } from "./socket/socket.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -26,7 +27,7 @@ app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`)
 });
