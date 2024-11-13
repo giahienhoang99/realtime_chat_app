1bb0ae6b6835817bef8b1eb3d71fac3c1c03d41d
hienhg <giahienhoang99@gmail.com>
2024-11-13 00:42:01 -0500

    messageContainer: use useAuthContext to welcome users instead of hardcoded value

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,6 +3,7 @@ import MessageInput from './MessageInput';
 import { TiMessages } from "react-icons/ti";
 import useConversation from '../../zustand/useConversation';
 import { useEffect } from 'react';
+import { useAuthContext } from '../../context/AuthContext';
 
 // message container aka chatbox
 const MessageContainer = () => {
@@ -41,12 +42,13 @@ export default MessageContainer;
 
 // no chat selected when first login/refresh page
 const NoChatSelected = () => {
+    const { authUser } = useAuthContext();
     return (
         <div className='flex items-center justify-center w-full h-full'>
             <div className='px-4 text-center sm:text-lg md:text-xl text-rose-400 font-semibold flex flex-col
              items-center gap-2'>
-                <p>Welcome to RealTimeChatApp</p>
-                <p>Select a chat to start messaging</p>
+                <p>Welcome {authUser.fullName} 😇</p>
+                <p>Select a chat to start messaging 🤙🏻</p>
                 <TiMessages className="text-3xl md:text-6xl text-center" />
             </div>
         </div>
