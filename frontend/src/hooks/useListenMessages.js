c56e1d4829664b31f42d53a2b75a3f70ac9743c7
hienhg <giahienhoang99@gmail.com>
2024-11-17 20:51:29 -0500

    shake new message in hook

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -8,8 +8,10 @@ const useListenMessages = () => {
 
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
+            newMessage.shouldShake = true;
             setMessages([...messages, newMessage]);
-        })
+        });
+
         return () => socket?.off("newMessage");
     },[socket, setMessages, messages]);
 };
