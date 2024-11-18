4140bd3dcc3039aac8893a6cc2fa54f3dee3afe0
hienhg <giahienhoang99@gmail.com>
2024-11-17 20:51:33 -0500

    new feat: new messages are shaking on receiver's end

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -12,8 +12,8 @@ const Message = forwardRef(({ message }, ref) => {
     const chatClassName = fromMe ? 'chat-end' : 'chat-start';
     const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
     const bubbleBgColor = fromMe ? 'bg-rose-400' : 'bg-black bg-opacity-60';
-    const formattedTime = extractTime(message.createdAt)
-
+    const formattedTime = extractTime(message.createdAt);
+    const shakeClass = message.shouldShake ? "shake" : "";
 
     return (
         <div>
@@ -25,7 +25,7 @@ const Message = forwardRef(({ message }, ref) => {
                             src={profilePic} />
                     </div>
                 </div>
-                <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+                <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass}`}>{message.message}</div>
                 <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
             </div>
         </div>
